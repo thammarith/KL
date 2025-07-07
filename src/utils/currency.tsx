a84cce928909ffdd862dabc2b3137abe63022d70
Thammarith Likittheerameth <thammarith.likittheerameth@agoda.com>
2025-07-07 19:49:20 +0700

    Update display
    Co-Authored-By: Thammarith <8011543+thammarith@users.noreply.github.com>

--- src/utils/currency.tsx
+++ src/utils/currency.tsx
@@ -1,9 +1,9 @@
-export const formatCurrency = (amount: number, currency: string): string => {
+export const formatCurrency = (amount: number, currency: string, currencyDisplay: Intl.NumberFormatOptions['currencyDisplay'] = 'symbol'): string => {
 	try {
 		return new Intl.NumberFormat(undefined, {
-			style: 'currency',
-			currencyDisplay: 'symbol',
+			currencyDisplay,
 			currency,
+			style: 'currency',
 		}).format(amount);
 	} catch {
 		return `${amount}`;
