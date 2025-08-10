f6f84d02a2b24d42fe00cae40d6cd0a567e3706a
Thammarith Likittheerameth <thammarith.likittheerameth@agoda.com>
2025-08-10 22:47:07 +0700

    Update logic
    Co-Authored-By: Thammarith <8011543+thammarith@users.noreply.github.com>

--- src/components/pwa/AutoUpdateToast.tsx
+++ src/components/pwa/AutoUpdateToast.tsx
@@ -1,11 +1,19 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'sonner';
+import { getItem, setItem, removeItem } from '@/utils/localStorage';
 
 const AutoUpdateToast = () => {
 	const { t } = useTranslation();
 
 	useEffect(() => {
+		const key = 'pwa.isJustUpdated';
+		const isJustUpdated = getItem<boolean>(key);
+		if (isJustUpdated) {
+			toast.success(t('appUpdated'), { duration: 3000 });
+			removeItem(key);
+		}
+
 		if ('serviceWorker' in navigator) {
 			navigator.serviceWorker.ready.then((registration) => {
 				registration.addEventListener('updatefound', () => {
@@ -16,17 +24,12 @@ const AutoUpdateToast = () => {
 								newWorker.state === 'installed'
 								&& navigator.serviceWorker.controller
 							) {
+								setItem(key, true);
 								newWorker.postMessage({ type: 'SKIP_WAITING' });
 								window.location.reload();
 							}
 						});
 					}
-				});
-			});
-
-			navigator.serviceWorker.addEventListener('controllerchange', () => {
-				toast.success(t('appUpdated'), {
-					duration: 3000,
 				});
 			});
 		}
